a488430ad5b34b9c05310ffda7855012c3d76b5e
Evgeny Vasiliev <evgnvas@gmail.com>
2022-08-30 11:44:09 +0300

    Убрать атрибуты, вернуть старую обработку аннотаций

--- src/AmbtaDoctrineEncryptBundle.php
+++ src/AmbtaDoctrineEncryptBundle.php
@@ -2,14 +2,12 @@
 
 namespace Ambta\DoctrineEncryptBundle;
 
-use JetBrains\PhpStorm\Pure;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Ambta\DoctrineEncryptBundle\DependencyInjection\DoctrineEncryptExtension;
 
 class AmbtaDoctrineEncryptBundle extends Bundle
 {
-    #[Pure]
     public function getContainerExtension(): ?ExtensionInterface
     {
         return new DoctrineEncryptExtension();
--- src/Configuration/Encrypted.php
+++ src/Configuration/Encrypted.php
@@ -2,17 +2,13 @@
 
 namespace Ambta\DoctrineEncryptBundle\Configuration;
 
-use Attribute;
-
 /**
  * The Encrypted class handles the @Encrypted annotation.
  *
  * @author Victor Melnik <melnikvictorl@gmail.com>
  * @Annotation
- * @Target("PROPERTY")
  */
-#[Attribute(Attribute::TARGET_PROPERTY)]
-class Encrypted implements Annotation
+class Encrypted
 {
     // Placeholder
 }
